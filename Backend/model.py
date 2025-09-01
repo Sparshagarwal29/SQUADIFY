bfea6699b414cd9544bc9efba4ea4b6ab2f76ea1
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2025-09-01 23:58:08 +0530

    sql adding

--- Backend/model.py
+++ Backend/model.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,ForeignKey
+from sqlalchemy.orm import relationship 
+
 from database import Base
 
 class User(Base):
@@ -8,17 +10,21 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
 
+class Team(Base):
+    __tablename__ = "teams"
+    id = Column(Integer, primary_key=True, index=True)
+    team_name = Column(String, index=True, nullable=False)
+    team_size = Column(Integer, nullable=False)
+    members = relationship("TeamMember", back_populates="team")
+
 class TeamMember(Base):
     __tablename__ = "team_members"
     id = Column(Integer, primary_key=True, index=True)
-    Name: Column(String, index=True)
-    Role: Column(String, index=True)
-    Email: Column(String, unique=True, index=True)
-    contact:  Column(String)
-    TechStack: Column(String)
-    Hobbies: Column(String, nullable=True)
+    name = Column(String, index=True, nullable=False)
+    role = Column(String, index=True, nullable=False)
+    email = Column(String, unique=True, index=True)
+    contact = Column(String, index=True)
+    tech_stack = Column(String)
+    hobbies = Column(String, nullable=True)
+    team_id = Column(Integer, ForeignKey("teams.id"))
     team = relationship("Team", back_populates="members")
-    teamSize: Column(String, index=True)
-    members: List[TeamMember]
-
-    
